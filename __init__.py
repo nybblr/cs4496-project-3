781ea19bf17287d39f75f93c8b767c05c662c6bf
Jonathan Martin <me@nybblr.com>
2013-03-27 12:30:54 -0400

    Cleanup!

--- __init__.py
+++ __init__.py
@@ -106,31 +106,6 @@ class World:
 			for shape in shapes:
 				shape.draw()
 
-			# for body in world.bodyList: # or: world.bodies
-				# # The body gives us the position and angle of its shapes
-				# for fixture in body.shapeList:
-				# 	# The fixture holds information like density and friction,
-				# 	# and also the shape.
-				# 	shape=fixture#.shape
-
-				# 	# Naively assume that this is a polygon shape. (not good normally!)
-				# 	# We take the body's transform and multiply it with each 
-				# 	# vertex, and then convert from meters to pixels with the scale
-				# 	# factor. 
-				# 	# vertices=[(body.GetXForm().R*v)*PPM for v in shape.vertices]
-				# 	vertices=[b2Mul(body.GetXForm(), v)*self.ppm for v in shape.vertices]
-
-				# 	# But wait! It's upside-down! Pygame and Box2D orient their
-				# 	# axes in different ways. Box2D is just like how you learned
-				# 	# in high school, with positive x and y directions going
-				# 	# right and up. Pygame, on the other hand, increases in the
-				# 	# right and downward directions. This means we must flip
-				# 	# the y components.
-				# 	vertices=[(v[0], self.height-v[1]) for v in vertices]
-
-				# 	pygame.draw.polygon(screen, (127,127,127,127), vertices)
-
-
 			# Instruct the world to perform a single step of simulation. It is
 			# generally best to keep the time step and iterations fixed.
 			world.Step(timeStep, iterations, iterations)
