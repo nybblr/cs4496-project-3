643d7fbeea3b7c8c65cbb030655d9f1d73e18778
Jonathan Martin <me@nybblr.com>
2013-04-01 11:25:48 -0400

    Level import from file. Sprites.

--- block.py
+++ block.py
@@ -1,11 +1,17 @@
 from shape import *
 
 class Block:
-	def __init__(self, game, position=(0,0), color=(0,0,0)):
+	def __init__(self, game, position=(0,0), color=(0,0,0), density=1.0):
 		self.game = game
 		self.position = position
 		self.color = color
-		self.shape = Shape(game, kind="box", color=color, position=position, params=(game.grid, game.grid))
+		self.shape = Shape(game,
+				kind = 'box',
+				color = color,
+				position = (position[0]*game.grid, position[1]*game.grid),
+				params = (game.grid, game.grid),
+				density = density,
+		)
 
 	def draw(self):
 		self.shape.draw()
--- level.py
+++ level.py
@@ -1,3 +1,23 @@
+import pygame
+from pygame.locals import *
+
+from block import *
+
 class Level:
-	def __init__(self):
+	def __init__(self, game):
 		self.blocks = list()
+		self.game = game
+
+	def initFromFile(self, filename, offset):
+		sprite = pygame.image.load(filename)
+
+		for x in range(sprite.get_width()):
+			for y in range(sprite.get_height()):
+				color = sprite.get_at((x, y))
+
+				if color[3] is not 0:
+					self.blocks.append(Block(
+						self.game,
+						(x+offset[0], y+offset[1]),
+						(color[0], color[1], color[2])
+					))
